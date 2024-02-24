e2b7aea1fdb35db2608c2a04574dfba03cd9fa5d
Perslay <natalia.44.fedyk@gmail.com>
2024-02-24 18:11:02 +0100

    Pagination
    Pictures limit per page is 40.
    Button for loading more pictures.
    Info displayed after reaching maximum requests (500).

--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,35 @@ import axios from 'axios';
 const form = document.querySelector('#search-form');
 const input = document.querySelector('#search-form input');
 const gallery = document.querySelector('.gallery');
+const moreButton = document.querySelector('.load-more');
+
+let currentPage = 1;
+let previousValue;
+let limit;
+
+moreButton.classList.add('hidden');
 
 form.addEventListener('submit', onSubmit);
 
 async function onSubmit(event) {
   event.preventDefault();
-  await getPictures();
+  moreButton.classList.add('hidden');
+
+  if (input.value === previousValue) {
+    currentPage++;
+    limit -= 40;
+    console.log(limit);
+  } else {
+    gallery.innerHTML = '';
+    currentPage = 1;
+    limit = 500;
+  }
+
+  await getPictures(currentPage);
+  previousValue = input.value;
 }
 
-async function getPictures() {
+async function getPictures(currentPage) {
   try {
     const response = await axios.get('https://pixabay.com/api/', {
       params: {
@@ -21,10 +41,17 @@ async function getPictures() {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
+        page: currentPage,
+        per_page: 40,
       },
     });
+
     const pictures = response.data.hits;
     showPictures(pictures);
+
+    if (!limit) {
+      limit = response.data.totalHits;
+    }
   } catch {
     Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
@@ -33,7 +60,6 @@ async function getPictures() {
 }
 
 function showPictures(pictures) {
-  gallery.innerHTML = '';
   pictures.forEach(picture => {
     const markup = `
       <div class="photo-card">
@@ -54,8 +80,29 @@ function showPictures(pictures) {
         </div>
       </div>
     `;
+
     gallery.insertAdjacentHTML('beforeend', markup);
+    moreButton.classList.remove('hidden');
   });
 }
 
-// zrobić paginację
+moreButton.addEventListener('click', loadMore);
+
+async function loadMore() {
+  try {
+    currentPage++;
+    await getPictures(currentPage);
+
+    if (limit <= 0) {
+      moreButton.classList.add('hidden');
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    } else {
+      limit -= 40;
+      console.log(limit);
+    }
+  } catch {
+    Notiflix.Notify.failure('Failed to load more photos');
+  }
+}
